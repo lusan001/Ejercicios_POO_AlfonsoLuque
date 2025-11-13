9fffc1fbf70dd5cafd3e9fbe8d8b536782959ae1
lusan001 <alfonsoluquesanchezjob@gmail.com>
2025-11-13 19:23:18 +0100

    Ejercicios Poo

--- ejer1.py
+++ ejer1.py
@@ -15,53 +15,78 @@ Crear duraciones de tiempos.
         A las duraciones de tiempo les puedo sumar y restar segundos.
         Las duraciones de tiempo se pueden sumar y restar.
 """
+
+
 class Duration:
+
     def __init__(self, hours=0, minutes=0, seconds=0):
-        total_seconds = hours * 3600 + minutes * 60 + seconds
-        self._hours = total_seconds // 3600
-        total_seconds %= 3600
-        self._minutes = total_seconds // 60
-        self._seconds = total_seconds % 60
+        self.__hours, self.__minutes, self.__seconds = hours, minutes, seconds
+        self.__normalize()
+
+    def __normalize(self):
+        """
+        Cambia las horas, minutos y segundos del objeto Duration para que estén en el rango de valores adecuados.
+        Pasamos todo a segundos, recalculamos las horas, minutos y segundos.
+        No admitimos duraciones de tiempo negativas, lanzamos excepción en ese caso.
+        """
+        seconds = self.__total_seconds()
+        if seconds < 0:
+            raise ValueError("No puede haber duraciones de tiempo negativas")
+        self.__hours = seconds // 3600
+        self.__minutes = seconds % 3600 // 60
+        self.__seconds = seconds % 3600 % 60
+
+    def __total_seconds(self):
+        return self.__hours * 3600 + self.__minutes * 60 + self.__seconds
 
     @property
     def hours(self):
-        return self._hours
+        return self.__hours
 
     @property
     def minutes(self):
-        return self._minutes
+        return self.__minutes
 
     @property
     def seconds(self):
-        return self._seconds
+        return self.__seconds
+
+    def __str__(self):
+        return f"{self.__hours}h {self.__minutes}m {self.__seconds}s"
 
     def __repr__(self):
-        return f"Duration({self.hours}, {self.minutes}, {self.seconds})"
+        return f"{self.__class__.__name__}({self.__hours}, {self.__minutes}, {self.__seconds})"
 
-    def __eq__(self, other):
-        if isinstance(other, Duration):
-            return (self.hours, self.minutes, self.seconds) == (other.hours, other.minutes, other.seconds)
-        return NotImplemented
-
-    def __lt__(self, other):
-        if isinstance(other, Duration):
-            return (self.hours, self.minutes, self.seconds) < (other.hours, other.minutes, other.seconds)
-        return NotImplemented
+    # Sobrecarga de operadores
 
     def __add__(self, other):
         if isinstance(other, Duration):
-            total_seconds = (self.hours + other.hours) * 3600 + (self.minutes + other.minutes) * 60 + (self.seconds + other.seconds)
-            return Duration(0, 0, total_seconds)
-        elif isinstance(other, int):
-            total_seconds = self.hours * 3600 + self.minutes * 60 + self.seconds + other
-            return Duration(0, 0, total_seconds)
-        return NotImplemented
+            return Duration(self.__hours + other.__hours, self.__minutes + other.__minutes,
+                            self.__seconds + other.__seconds)
+        else:
+            return Duration(self.__hours, self.__minutes, self.__seconds + other)
 
     def __sub__(self, other):
         if isinstance(other, Duration):
-            total_seconds = (self.hours - other.hours) * 3600 + (self.minutes - other.minutes) * 60 + (self.seconds - other.seconds)
-            return Duration(0, 0, total_seconds)
-        elif isinstance(other, int):
-            total_seconds = self.hours * 3600 + self.minutes * 60 + self.seconds - other
-            return Duration(0, 0, total_seconds)
-        return NotImplemented
+            return Duration(self.__hours - other.__hours, self.__minutes - other.__minutes,
+                            self.__seconds - other.__seconds)
+        else:
+            return Duration(self.__hours, self.__minutes, self.__seconds - other)
+
+    def __eq__(self, other):
+        return (self.__hours, self.__minutes, self.__seconds) == (other.__hours, other.__minutes, other.__seconds)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        return self.__total_seconds() < other.__total_seconds()
+
+    def __le__(self, other):
+        return self.__total_seconds() <= other.__total_seconds()
+
+    def __gt__(self, other):
+        return not self <= other
+
+    def __ge__(self, other):
+        return not self < other
