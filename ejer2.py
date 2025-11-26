7dc873d68820a9dfe6bc4c182349c4136373d5f8
lusan001 <alfonsoluquesanchezjob@gmail.com>
2025-11-26 09:50:00 +0100

    update

--- ejer2.py
+++ ejer2.py
@@ -10,7 +10,7 @@ Ojo!!! No se puede tener un denominador cero.
         Sumar, multiplicar, dividir, restar.
         ¡Ojo!!! Esto se puede hacer: f + 1, 5 * f
     Las fracciones se pueden comparar.
-        ¡==, <, <=, >, >=, !=
+        ¡==, <, <=, >, >=,!=
         Ojo!!! ¡Estas dos fracciones son iguales: 1/2 y 2/4
         Ojo!!! Esto se puede hacer 1 < 1/2
 """
@@ -22,18 +22,115 @@ class Fraction:
     Clase que representa fracciones (numerador / denominador).
     - Se simplifican automáticamente.
     - No se permite denominador 0.
-    - Soporta operaciones con otras Fraction y con enteros (int).
-    - Soporta comparaciones con otras Fraction y con enteros.
+    - Soporta suma con Fraction y con int (incluye int + Fraction).
     """
 
     def __init__(self, numerator, denominator=1):
         if denominator == 0:
-            raise ValueError("El denominador no puede ser 0.")
-        # Normalizar signo: el denominador siempre será positivo
-        sign = -1 if numerator * denominator < 0 else 1
-        n, d = abs(numerator), abs(denominator)
-        common = gcd(n, d)
-        # Guardamos la fracción ya simplificada
-        self.__num = sign * (n // common)
-        self.__den = d // common
+            raise ZeroDivisionError("El denominador no puede ser 0.")
+
+        if denominator < 0:
+            numerator = -numerator
+            denominator = -denominator
+
+        # Simplificar la fracción
+        g = gcd(abs(numerator), abs(denominator))
+        self.numerator = numerator // g
+        self.denominator = denominator // g
+
+    def __repr__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def __add__(self, other):
+        if isinstance(other, Fraction):
+            new_numerator = (self.numerator * other.denominator +
+                             other.numerator * self.denominator)
+            new_denominator = self.denominator * other.denominator
+        elif isinstance(other, int):
+            new_numerator = self.numerator + other * self.denominator
+            new_denominator = self.denominator
+        else:
+            return NotImplemented
+        return Fraction(new_numerator, new_denominator)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, Fraction):
+            new_numerator = (self.numerator * other.denominator -
+                             other.numerator * self.denominator)
+            new_denominator = self.denominator * other.denominator
+        elif isinstance(other, int):
+            new_numerator = self.numerator - other * self.denominator
+            new_denominator = self.denominator
+        else:
+            return NotImplemented
+        return Fraction(new_numerator, new_denominator)
+
+    def __rsub__(self, other):
+        if isinstance(other, int):
+            new_numerator = other * self.denominator - self.numerator
+            new_denominator = self.denominator
+            return Fraction(new_numerator, new_denominator)
+        else:
+            return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, Fraction):
+            new_numerator = self.numerator * other.numerator
+            new_denominator = self.denominator * other.denominator
+        elif isinstance(other, int):
+            new_numerator = self.numerator * other
+            new_denominator = self.denominator
+        else:
+            return NotImplemented
+        return Fraction(new_numerator, new_denominator)
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Fraction):
+            new_numerator = self.numerator * other.denominator
+            new_denominator = self.denominator * other.numerator
+        elif isinstance(other, int):
+            new_numerator = self.numerator
+            new_denominator = self.denominator * other
+        else:
+            return NotImplemented
+        return Fraction(new_numerator, new_denominator)
+    def __rtruediv__(self, other):
+        if isinstance(other, int):
+            new_numerator = other * self.denominator
+            new_denominator = self.numerator
+            return Fraction(new_numerator, new_denominator)
+        else:
+            return NotImplemented
+    def __eq__(self, other):
+        if isinstance(other, Fraction):
+            return (self.numerator == other.numerator and
+                    self.denominator == other.denominator)
+        elif isinstance(other, int):
+            return self.numerator == other * self.denominator
+        else:
+            return NotImplemented
+    def __lt__(self, other):
+        if isinstance(other, Fraction):
+            return (self.numerator * other.denominator <
+                    other.numerator * self.denominator)
+        elif isinstance(other, int):
+            return self.numerator < other * self.denominator
+        else:
+            return NotImplemented
+    def __le__(self, other):
+        return self < other or self == other
+    def __gt__(self, other):
+        return not (self <= other)
+    def __ge__(self, other):
+        return not (self < other)
+    def __ne__(self, other):
+        return not (self == other)
 
